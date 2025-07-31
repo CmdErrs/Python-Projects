b0c2dabeea3f63b4a372aa06d9b174f31bd056e2
CmdErrs <cmd.errs@gmail.com>
2025-08-01 09:04:00 +1000

    corrected if statements

--- if.py
+++ if.py
@@ -7,8 +7,8 @@ elif num < 5 :
 else :
     print( 'Number is 5' )
 
-    if num > 7 and num < 9 :
+if num > 7 and num < 9 :
         print( 'Number is 8' )
-    if num == 1 or num == 3 :
+if num == 1 or num == 3 :
         print( 'Number is 1 or 3' )
         
