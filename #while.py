c761dee5281498eb5d89392bc2705ef31fe41e11
CmdErrs <cmd.errs@gmail.com>
2025-08-01 15:58:40 +1000

    while added

---
+++ #while.py
@@ -0,0 +1,10 @@
+#while
+
+i = 1
+while i < 4 :
+    print( 'Outer Loop iteration:' , i )
+    i += 1
+    j = 1
+    while j < 4 :
+        print( '\tInner Loop Iteration' , j )
+        j += 1
--- for.py
+++ for.py
@@ -1,22 +1,30 @@
+# for loop doesnt allow step size and end to be specified
 
-chars = [ 'A' , 'B' , 'C' ] #list
-fruit = ( 'Apple' , 'Banana' , 'Cherry' ) #tuple - immutable
-info = { 'name' : 'Mike' , 'ref' : 'Python' , 'sys' : 'Win' } #set - unique elements & functions
+# range() can generate a sequence that decreases, counting down, as well as those that count upward
+# range(5) iterates through -> 0,1,2,3,4 elements
+# range(1,5) interates starting at element -> 1,2,3,4
+# range(1,14,4) step by 4 -> 1,5,9,13
 
-print( 'Elements: \t' , end = '' )
-for item in chars:
-    print( item , end = '')
+
+
+chars = [ 'A' , 'B' , 'C' , 'D' ] #list
+fruit = ( 'Apple' , 'Banana' , 'Cherry' , 'Duran' ) #tuple - immutable
+info = { 'name' : 'Mike' , 'ref' : 'Python' , 'sys' : 'Win' , 'new' : 'set item' } #set - unique elements & functions
+
+print( 'Elements: ' , end = '' )           # end = '' keeps next line with previous
+for elements in chars:
+    print( elements , end = '')            # end = '' keeps all elements on same line
 
 print( '\nEnumerated:\t' , end = '')
-for item in enumerate( chars ) :
+for item in enumerate( chars ) :             # enumerate() displays each elements index number and its value (0, 'A')(1, 'B')(2, 'C')(3, 'D')
     print( item , end = '' )
 
-print( '\nZipped: \t' , end = '' )
-for item in zip( chars, fruit ) :
+print( '\nZipped: \t' , end = '' )          
+for item in zip( chars, fruit , info ) :   # zip() returns same index number value for each list together. ('A', 'Apple', 'name')('B', 'Banana', 'ref')('C', 'Cherry', 'sys')
     print( item, end = '' )
 
 print( '\nPaired:' )
-for key , value in info.items() :
+for key , value in info.items() :          # when looping through a dictionary, to display each key and its value use the dictionary items() method, and specify key name and value.
     print( key , '=' , value )
 
 
