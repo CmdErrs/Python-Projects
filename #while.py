bdbadc46565f71c4a95a39d35d30360eb408f727
CmdErrs <cmd.errs@gmail.com>
2025-08-02 18:50:51 +1000

    break & while files added

--- #while.py
+++ #while.py
@@ -1,4 +1,7 @@
 #while
+# += is short hand for ( i = i + 1 )
+
+
 
 i = 1
 while i < 4 :
@@ -8,3 +11,12 @@ while i < 4 :
     while j < 4 :
         print( '\tInner Loop Iteration' , j )
         j += 1
+
+
+
+# Fibonacci loop
+a = b = 1
+while b < 100:
+    print( b )
+    a , b = b , a + b
+
