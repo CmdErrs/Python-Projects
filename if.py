729c93620ceb55b3035228e0c4bbe4068205fee2
CmdErrs <cmd.errs@gmail.com>
2025-09-09 11:39:34 +1000

    test

---
+++ if.py
@@ -0,0 +1,11 @@
+#Controlling Blocks
+
+num = int( input( 'Please Enter A Number: '))
+
+if num > 5 :
+        print( 'Number Exceeds 5')
+elif num < 5 :
+        print( 'Number is Less Than 5' )
+else :
+        print( 'Number is 5' )
+        
